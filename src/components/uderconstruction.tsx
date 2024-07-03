11ea88b77977ca56bc147ca0724d52072bef29ac
enoskyeza <enoskyeza@gmail.com>
2024-07-03 18:00:44 +0300

    Add maintenance page

--- src/components/uderconstruction.tsx
+++ src/components/uderconstruction.tsx
@@ -1,8 +1,46 @@
+import Image from "next/image";
 
 export default function Uderconstruction() {
-  return (
-      <>
-          <h1>Page under constructions</h1>
-      </>
-  );
+    return (
+        <>
+            <div className="w-full h-screen flex flex-col items-center justify-between bg-white">
+                <div className="xl:w-1/2 flex-1 flex flex-col items-center justify-center text-center px-4 lg:px-0">
+
+                    <div
+                        className="w-full flex items-center justify-center lg:justify-start absolute top-4 lg:left-0">
+                        <Image src='/logo.jpg' alt="construction-image" width={300} height={250}
+                               className=''></Image>
+                    </div>
+                    <Image src='/construction.svg' alt="construction-image" width={500} height={500}
+                           className='mb-4 mt-32 ' ></Image>
+                    <p className="text-4xl font-bold text-amber-700 capitalize tracking-wide mt-8">Website under
+                        maintenance</p>
+                    <p className="text-xl text-black uppercase mt-4">We'll be back soon!!</p>
+                </div>
+                <div className="w-full py-4 border-t-2 border-amber-900 bg-brand-primary">
+                    <div
+                        className="container mx-auto flex flex-col md:flex-row items-center justify-center text-white text-sm md:space-x-8 space-y-1 md:space-y-0">
+                        <span className="font-bold">You can contact us:</span>
+                        <a href="#" className="flex items-center space-x-1" target="_blank" title="Call">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20"
+                                 fill="currentColor">
+                                <path
+                                    d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
+                            </svg>
+                            <span>+256 393208189 | +256 783 003098</span>
+                        </a>
+                        <a href="#" className="flex items-center space-x-1" target="_blank" title="Email">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20"
+                                 fill="currentColor">
+                                <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                                <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                            </svg>
+                            <span>info@real.ug</span>
+                        </a>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    );
 }
