3a5c3f5a00f020dbd5b447ad6f6cdb6bda37efc4
enoskyeza <enoskyeza@gmail.com>
2024-07-29 13:29:34 +0300

    modify section titles

--- src/components/Common/SectionTitle.tsx
+++ src/components/Common/SectionTitle.tsx
@@ -17,7 +17,8 @@ const SectionTitle = ({
         className={`w-full ${center ? "mx-auto text-center" : ""}`}
         style={{ maxWidth: width, marginBottom: mb }}
       >
-        <h2 className="mb-4 text-3xl font-bold !leading-tight brand-text-primary sm:text-4xl md:text-[45px]">
+        {/*sm:text-4xl md:text-[45px]*/}
+        <h2 className="mb-4 text-3xl font-bold !leading-tight brand-text-primary sm:text-4xl">
           {title}
         </h2>
         <p className="text-base !leading-relaxed text-body-color md:text-lg">
