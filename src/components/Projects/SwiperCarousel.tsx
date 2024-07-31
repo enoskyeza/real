4fa6ae4d5d2d4b14519a3f8eaeed02aa79ce5678
enoskyeza <enoskyeza@gmail.com>
2024-07-31 13:21:58 +0300

    Test swiper carousel cards

--- src/components/Projects/SwiperCarousel.tsx
+++ src/components/Projects/SwiperCarousel.tsx
@@ -6,6 +6,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import '@/components/Projects/item.css'
 
 // import SwiperCore, { Navigation, Pagination } from 'swiper';
 
@@ -25,11 +26,23 @@ const SwiperCarousel = () => {
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
+      <SwiperSlide>
+        <div className="carousel-item">
+          <img
+              className="carousel-item__img"
+              src="https://images.pexels.com/photos/708392/pexels-photo-708392.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+              alt="people"
+          />
+          <div className="carousel-item__details">
+            <div className="controls">
+              <span className="fas fa-play-circle"></span>
+              <span className="fas fa-plus-circle"></span>
+            </div>
+            <h5 className="carousel-item__details--title">Descriptive Title</h5>
+            <h6 className="carousel-item__details--subtitle">Date and Duration</h6>
+          </div>
+        </div>
+      </SwiperSlide>
     </Swiper>
   );
 };
