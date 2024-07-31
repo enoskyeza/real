fa5978c123cab6b0d36a73c26394b62379211709
enoskyeza <enoskyeza@gmail.com>
2024-07-31 12:27:08 +0300

    Add swiper styles

--- src/components/Projects/SwiperCarousel.tsx
+++ src/components/Projects/SwiperCarousel.tsx
@@ -1,10 +1,11 @@
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-
+// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 // import SwiperCore, { Navigation, Pagination } from 'swiper';
 
