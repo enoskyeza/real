b536ab04301c72af2b19f63107cf9f02473b3906
enoskyeza <enoskyeza@gmail.com>
2024-07-31 12:26:34 +0300

    Implement swiper package

---
+++ src/components/Projects/SwiperCarousel.tsx
@@ -0,0 +1,12 @@
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import SwiperCore, { Navigation, Pagination } from 'swiper';
+
+// Install Swiper modules
+// SwiperCore.use([Navigation, Pagination]);
