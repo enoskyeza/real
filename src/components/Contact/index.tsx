fd19d468fae4a54ace3f4d40d6d6522df7181869
enoskyeza <enoskyeza@gmail.com>
2024-08-02 04:23:13 +0300

    Modify contacts section

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,29 +1,39 @@
 import NewsLatterBox from "./NewsLatterBox";
+import Image from "next/image";
 
 const Contact = () => {
   return (
-    <section id="contact" className="overflow-hidden pt-16 md:pt-20 lg:pt-28 flex justify-center">
+    <section id="contact" className="overflow-hidden pb-16 md:pb-20 lg:pb-28 flex justify-center">
       <div className="container">
         <div className="-mx-4 flex flex-wrap">
-          <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
+          <div className="relative w-full px-4 lg:w-5/12 xl:w-1/2">
+                <Image
+                  src="/images/contact/contact-1.png"
+                  alt="contact-us-image"
+                  fill
+                  className="max-w-full lg:mr-0"
+                />
+          </div>
+          <div className="w-full px-4 lg:w-7/12 xl:w-1/2 pt-16 md:pt-20 lg:pt-28">
+              <h2 className="mb-3 text-2xl font-bold text-black  sm:text-3xl lg:text-2xl xl:text-3xl">
+                Get in touch with our team.
+              </h2>
+              <p className="mb-12 text-base font-medium brand-text-primary">
+                Let&apos;s work together!
+              </p>
+
             <div
-              className="mb-12 rounded-sm bg-gray-50 px-8 py-11 shadow-three sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
+              className="mb-12 rounded-lg bg-brand-dark px-8 py-11 shadow-xl sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
               data-wow-delay=".15s
               "
             >
-              <h2 className="mb-3 text-2xl font-bold text-black  sm:text-3xl lg:text-2xl xl:text-3xl">
-                Need Help? Leave a message.
-              </h2>
-              <p className="mb-12 text-base font-medium text-body-color">
-                Our team will get back to you ASAP via email.
-              </p>
               <form>
                 <div className="-mx-4 flex flex-wrap">
                   <div className="w-full px-4 md:w-1/2">
                     <div className="mb-8">
                       <label
                         htmlFor="name"
-                        className="mb-3 block text-sm font-medium text-dark"
+                        className="mb-3 block text-sm font-medium text-white"
                       >
                         Your Name
                       </label>
@@ -38,7 +48,7 @@ const Contact = () => {
                     <div className="mb-8">
                       <label
                         htmlFor="email"
-                        className="mb-3 block text-sm font-medium text-dark"
+                        className="mb-3 block text-sm font-medium text-white"
                       >
                         Your Email
                       </label>
@@ -54,7 +64,7 @@ const Contact = () => {
                     <div className="mb-8">
                       <label
                         htmlFor="message"
-                        className="mb-3 block text-sm font-medium text-dark"
+                        className="mb-3 block text-sm font-medium text-white"
                       >
                         Your Message
                       </label>
@@ -70,15 +80,12 @@ const Contact = () => {
                   <div className="w-full px-4">
                     <button className="rounded-sm bg-brand-primary px-9 py-4 text-base font-medium text-white shadow-submit
                     duration-300 hover:bg-primary/90">
-                      Submit Ticket
+                      Submit
                     </button>
                   </div>
                 </div>
               </form>
             </div>
-          </div>
-          <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
-            <NewsLatterBox />
           </div>
         </div>
       </div>
