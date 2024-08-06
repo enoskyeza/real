8c00f4ac978209ef7bbcf3288126acaca0b0500a
enoskyeza <enoskyeza@gmail.com>
2024-08-06 13:13:07 +0300

    modify cta for services

--- src/components/Services/SingleService.tsx
+++ src/components/Services/SingleService.tsx
@@ -5,15 +5,15 @@ const SingleService = ({ service }: { service: Service }) => {
   const { title, image, description } = service;
 
   return (
-      <div className="w-full flex flex-col bg-white rounded overflow-hidden shadow-lg p-4  duration-300 hover:shadow-one lg:px-5 xl:px-8">
+      <div className="w-full flex flex-col bg-white rounded overflow-hidden shadow-lg p-4  duration-300 hover:shadow-one">
         <div className="mb-2 flex items-center space-x-1">
           <Image src={image} alt={title} width={80} height={80} className="rounded-full" />
         </div>
         <h3 className="mb-4 text-lg font-semibold text-dark lg:text-base xl:text-lg">{title}</h3>
-        <p className="pb-8 text-base leading-relaxed text-body-color">
+        <p className="pb-8 text-sm leading-relaxed text-body-color">
           {description}
         </p>
-        <a className="pt-4 border-t border-body-color border-opacity-10 mt-auto" href='#'>Request quote</a>
+        <a className="pt-4 border-t border-body-color border-opacity-10 mt-auto" href='#'>Request Quote</a>
       </div>
   );
 };
