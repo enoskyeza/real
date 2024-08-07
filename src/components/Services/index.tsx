f5d48a8af6098394904f2f6a224b60062b282c5f
enoskyeza <enoskyeza@gmail.com>
2024-08-07 05:10:33 +0300

    add motion to service elements

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -6,25 +6,25 @@ import {services} from "@/components/data";
 import './services.css'; // Import the CSS file
 import {Parallax} from 'react-scroll-parallax';
 import Image from "next/image";
-import { motion } from 'framer-motion';
+import {motion} from 'framer-motion';
 
 
 const containerVariants = {
-  hidden: { opacity: 0, x: -50 },
-  visible: {
-    opacity: 1,
-    x: 0,
-    transition: {
-      duration: 0.5,
-      when: "beforeChildren",
-      staggerChildren: 0.3,
+    hidden: {opacity: 0, x: -50},
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            duration: 0.5,
+            when: "beforeChildren",
+            staggerChildren: 0.3,
+        },
     },
-  },
 };
 
 const itemVariants = {
-  hidden: { opacity: 0, y: 20 },
-  visible: { opacity: 1, y: 0 },
+    hidden: {opacity: 0, y: 20},
+    visible: {opacity: 1, y: 0},
 };
 
 
@@ -34,34 +34,48 @@ const Services = () => {
             id="services"
             className="relative py-16 md:pb-0 md:pt-20 lg:py-28 flex items-center justify-center"
         >
-            <div className="absolute inset-0 w-full h-full md:h-3/5 overflow-hidden" >
-            <Parallax translateY={[-30, 30]} className="relative h-full w-full">
-                        <Image
-                            src="/images/background/bg-1.jpg"
-                            alt="Background"
-                            layout="fill"
-                            objectFit="cover"
-                            objectPosition="center"
-                            className="w-full h-full"
-                        />
-                <div className="absolute inset-0 bg-orange-300 opacity-80"></div>
-            </Parallax>
+            <div className="absolute inset-0 w-full h-full md:h-3/5 overflow-hidden">
+                <Parallax translateY={[-30, 30]} className="relative h-full w-full">
+                    <Image
+                        src="/images/background/bg-1.jpg"
+                        alt="Background"
+                        layout="fill"
+                        objectFit="cover"
+                        objectPosition="center"
+                        className="w-full h-full"
+                    />
+                    <div className="absolute inset-0 bg-orange-300 opacity-80"></div>
+                </Parallax>
             </div>
 
             <div className="container custom-container content">
-                <SectionTitle
-                    title="Below are some of our services"
-                    subtitle="Our Services"
-                    paragraph=" "
-                    center
-                    subtitle_color="white"
-                />
+                <motion.div
+                    initial="hidden"
+                    animate="visible"
+                    variants={containerVariants}
+                >
+                    <SectionTitle
+                        title="Below are some of our services"
+                        subtitle="Our Services"
+                        paragraph=" "
+                        center
+                        subtitle_color="white"
+                    />
+                </motion.div>
+
                 <div className="relative">
-                    <div className="grid grid-cols-1 gap-x-4 gap-y-10 md:grid-cols-3 lg:grid-cols-6">
+                    <motion.div
+                        className="grid grid-cols-1 gap-x-4 gap-y-10 md:grid-cols-3 lg:grid-cols-6"
+                        initial="hidden"
+                        animate="visible"
+                        variants={containerVariants}
+                    >
                         {services.map((service) => (
-                            <SingleService key={service.id} service={service}/>
+                            <motion.div key={service.id} variants={itemVariants}>
+                                <SingleService service={service}/>
+                            </motion.div>
                         ))}
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </section>
