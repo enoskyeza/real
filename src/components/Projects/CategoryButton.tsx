24db206e75a0d09bcba724979e1986c34b619388
enoskyeza <enoskyeza@gmail.com>
2024-08-07 06:32:17 +0300

    Implement category filtering

--- src/components/Projects/CategoryButton.tsx
+++ src/components/Projects/CategoryButton.tsx
@@ -15,7 +15,7 @@ const CategoryButtons: React.FC<CategoryButtonsProps> = ({ categories, activeCat
           type="button"
           className={`${
             activeCategory === category ? "bg-brand-dark text-white" : "text-gray-900 border-gray-200 bg-gray-100"
-          } hover:text-white hover:bg-orange-900 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3`}
+          } hover:text-white hover:bg-orange-900 focus:ring-4 focus:outline-none focus:ring-orange-100 rounded-full text-base font-medium px-5 py-2.5 text-center me-3 mb-3`}
           onClick={() => setActiveCategory(category)}
         >
           {category}
