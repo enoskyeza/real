fc07c39b946ad562f1c4b711af8fe31084df487f
enoskyeza <enoskyeza@gmail.com>
2024-08-08 23:35:03 +0300

    Add animation variants and useInview hook

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -4,8 +4,26 @@ import SingleTestimonial from "./SingleTestimonial";
 import { motion } from "framer-motion"
 import { useInView } from "react-intersection-observer";
 
+const containerVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      when: "beforeChildren",
+      staggerChildren: 0.2,
+    },
+  },
+};
 
 const Testimonials = () => {
+
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
   return (
     <section className="dark:bg-bg-color-dark bg-orange-100 relative z-10 py-16 md:py-20 lg:py-28 flex justify-center">
       <div className="container custom-container">
